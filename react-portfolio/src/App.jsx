8bc09a93c9b2a28efbff9c7f83ba002b13df89d4
Jaspreet Singh <115124290+jaz404@users.noreply.github.com>
2024-06-04 04:41:03 -0600

    animations, contact me button

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,6 +1,6 @@
 
 import styles from './App.module.css';
-import { Hero } from './components/Navbar/Hero/Hero';
+import { Hero } from './components/Hero/Hero';
 import { Navbar } from './components/Navbar/Navbar';
 
 
